345d94d5d2a23d8e57ceb5c0b5f2127b8705b054
Mark Polyakov <mark@markasoftware.com>
2020-03-11 22:20:59 -0700

    touch up flash

--- flash.sh
+++ flash.sh
@@ -2,8 +2,6 @@
 
 src="$1"
 echo "Compile..."
-avr-gcc -Os -DF_CPU=1000000 -mmcu=attiny85 -c "$src"
-echo "Link? ..."
-avr-gcc -o "${src/.c/.elf}" -DF_CPU=1000000 -mmcu=attiny85 "${src/.c/.o}"
+avr-gcc -o "${src/.c/.elf}" -Os -DF_CPU=1000000 -mmcu=attiny85 "$src"
 echo "Flash..."
-avrdude -c avrisp -b 19200 -p attiny85 -P /dev/ttyACM0 -U flash:w:"${src/.c/.elf}"
+avrdude -c avrisp -b 19200 -p attiny85 -P /dev/ttyACM? -U flash:w:"${src/.c/.elf}"
